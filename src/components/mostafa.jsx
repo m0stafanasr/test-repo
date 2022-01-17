fe735eeb74b629e0ec1598040f3e11f28b7918c6
MoSTaFaMahmed <mostafamohamef200@gmail.com>
2022-01-18 01:46:23 +0200

    mostafaBranch second commit

--- src/components/mostafa.jsx
+++ src/components/mostafa.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 const Mostafa = () => {
-     return ( <>
-     <h1>Mostafa Branch</h1>
-     </> );
-}
- 
+  return (
+    <>
+      <h1>Mostafa Branch</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugiat
+        neque adipisci, quo et non vero officiis laborum commodi distinctio
+        dolore voluptatibus asperiores veniam architecto qui, voluptates nisi
+        odit. Illo
+      </p>
+    </>
+  );
+};
+
 export default Mostafa;
